0f4fb455b80bfc20ccaa763839cc89ba4e0f9073
Damian Pieczynski <piecyk@gmail.com>
2019-01-30 22:13:08 +0100

    Update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ const jest = {
   files: ['src/**/*.test.{ts,tsx}'],
   env: {
     jest: true,
+    node: true,
+    browser: false,
   },
 }
 
@@ -23,16 +25,16 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   extends: [
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
 
     'plugin:import/errors',
     'plugin:import/warnings',
-
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/react',
   ],
   plugins: ['react-hooks'],
   parserOptions: {
@@ -54,13 +56,24 @@ module.exports = {
     },
   },
   rules: {
-    'react-hooks/rules-of-hooks': 2,
     'no-console': 0,
+    'no-var': 2,
+    'no-void': 2,
+    'no-with': 2,
+    'object-shorthand': 1,
+    'prefer-arrow-callback': 2,
+    'prefer-const': 2,
+    'prefer-rest-params': 2,
+    'prefer-spread': 2,
+
+    'react-hooks/rules-of-hooks': 2,
+
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/member-delimiter-style': 0,
     '@typescript-eslint/indent': [2, 2],
     '@typescript-eslint/member-ordering': 2,
     '@typescript-eslint/explicit-member-accessibility': 0,
+
     'import/first': 2,
     'import/newline-after-import': 2,
   },
